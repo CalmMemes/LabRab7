48cb01644e4d66e66acf91b17de44361aa43db9a
CalmMemes <monsterhoma@gmail.com>
2020-12-13 14:35:41 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabRab7
+{
+    class Class1
+    {
+        double a, b;
+        private Class1() { }
+        public Class1(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public void Cout()
+        {
+            Console.WriteLine("Пользователь ввел данные: a = {0}, b = {1}", a, b);
+
+            Console.WriteLine("Интеграл равен: {0}", Integral());
+
+            Console.WriteLine("Длина отрезка: {0:0.000}", LengthSection());
+        }
+        public void Cout(ConsoleColor fg, ConsoleColor bgs)
+        {
+            Console.ForegroundColor = fg;
+            Console.BackgroundColor = bgs;
+            Console.Clear();
+            Cout();
+        }
+
+        private double Integral()
+        {
+            double integral = (0.5 * -(Math.Pow(b, 2))) - (0.5 * -(Math.Pow(a, 2)));
+            return integral;
+        }
+
+        private double LengthSection()
+        {
+            double dlOtrez = Math.Sqrt(Math.Pow(((-b) - (-a)), 2) + Math.Pow((b - a), 2));
+            return dlOtrez;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabRab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"output.txt");
+            var new_in = new StreamReader(@"input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+#endif
+
+#if DEBUG
+            var lala = new Class1(2, 5);
+            lala.Cout(ConsoleColor.Cyan, ConsoleColor.Black);
+#endif
+
+#if !DEBUG
+
+            var p1 = new Class1(4.5, 10.2);
+            p1.Cout(ConsoleColor.Cyan, ConsoleColor.Black);
+
+#endif
+
+#if !DEBUG
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+#endif
+
+#if DEBUG
+            Console.ReadKey();
+#endif
+
+
+        }
+    }
+}
